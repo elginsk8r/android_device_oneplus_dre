41a18a64db60ea082930414eb341eef319b34df0
Nick Reuter <nreuter85@gmail.com>
2024-02-21 17:10:19 -0600

    dre: inherit common usb component
    Change-Id: Ib76c739ec2a8f83d2a3bf4d5bcf1a3e579a554b5

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     media \
     perf \
+    usb \
     wfd
 $(call inherit-product, device/qcom/common/build/target/holi.mk)
 
@@ -391,13 +392,6 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
 # USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
