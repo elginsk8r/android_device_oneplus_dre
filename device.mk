e2a11b21fccafc74cb68e95594a0b0c704c496b2
Nick Reuter <nreuter85@gmail.com>
2024-02-28 18:47:42 -0600

    dre: use common gnss components

--- device.mk
+++ device.mk
@@ -22,6 +22,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     alarm \
     charging \
     display \
+    gps \
     media \
     perf \
     wfd
@@ -199,18 +200,6 @@ PRODUCT_COPY_FILES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
-
-# GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss.measurement_corrections@1.1.vendor \
-    android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Health
 PRODUCT_PACKAGES += \
