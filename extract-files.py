5893623c95573f050cc8a43225dcc486215de465
SagarMakhar <sagarmakhar@gmail.com>
2024-10-01 23:59:30 +0530

    dre: Turn off persist.vendor.radio.poweron_opt
    It's set to 1 in qcril_database migrations. Sed the property
    to something else so it defaults to 0.
    
    When the value is 1 it breaks SMS receiving without QTI telephony.
    
    Change-Id: I7f56ecaac51abb6525c6022d92eac1c285ab0688

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('IGNORED_IRQ=19,21,38$', 'IGNORED_IRQ=19,21,38,209,218'),
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so'),
+    'vendor/lib64/libril-db.so': blob_fixup()
+        .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
